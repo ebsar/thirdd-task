c78e32282b64f63386e189e47852915d7ec1c03b
Ebsar <ebsar@Ebsars-MacBook-Pro.local>
2022-06-08 02:09:28 +0200

    deploy

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -7,7 +7,7 @@ function Modal({ onClose }) {
     setChangeTheme((prev) => !prev);
   };
   const [Color, setColor] = useState("#0");
-  const [Color1, setColor1] = useState("#ffffff");
+  const [Color1, setColor1] = useState("#0");
   const [Color2, setColor2] = useState("#0");
   const [Color3, setColor3] = useState("#0");
   const [Color4, setColor4] = useState("#0");
@@ -33,127 +33,127 @@ function Modal({ onClose }) {
   };
   return (
     <div style={{ backgroundColor: Color1 }} className="backrground-container">
-      <div className="Container1">
-        <div className="First-Container">
-          <p>Theme Color</p>
-          <p onClick={onClicker}>Change Theme</p>
-        </div>
-      {/*   {ChangeTheme && (
-          <div
-            style={{
-              backgroundColor: Color1,
-            }}
-            className="Modal2-Container"
-          >
-            <div className="Text-1">
-              <div className="text-4-1">
-                <p style={{ color: Color }}>Font Color</p>
-                <p style={{ color: Color }}>{Color}</p>
-              </div>
-              <button
-                style={{ backgroundColor: Color }}
-                className="text-1-btn"
-                onClick={onClick}
-              ></button>
+      <div className="First-Container">
+        <p style={{ color: Color }}>Theme Color</p>
+        <p style={{ color: Color2 }} onClick={onClicker} >Change Theme</p>
+      </div>
+      {ChangeTheme && (
+        <div
+          style={{
+            backgroundColor: Color1,
+          }}
+          className="Modal2-Container"
+        >
+          <div className="Text-1">
+            <div className="text-4-1">
+              <p style={{ color: Color }}>Font Color</p>
+              <p style={{ color: Color }}>{Color}</p>
             </div>
-            <div className="Color1">
-              {colorpopup && (
-                <CirclePicker
-                  color={Color}
-                  onChangeComplete={(Color) => {
-                    setColor(Color.hex);
-                  }}
-                />
-              )}
-            </div>
-            <div style={{ Color }} className="Text-2">
-              <div className="text-4-1">
-                <p style={{ color: Color }}>Backrground Color</p>
-                <p style={{ color: Color }}>{Color1}</p>
-              </div>
-              <button
-                className="text-2-btn"
-                style={{ backgroundColor: Color1 }}
-                onClick={onClick1}
-              ></button>
-            </div>
-            <div className="Color1">
-              {colorpopup1 && (
-                <CirclePicker
-                  Color1={Color1}
-                  onChangeComplete={(Color1) => {
-                    setColor1(Color1.hex);
-                  }}
-                />
-              )}
-            </div>
-            <div className="Text-3">
-              <div className="text-4-1">
-                <p style={{ color: Color }}>Button Color</p>
-                <p style={{ color: Color }}>{Color2}</p>
-              </div>
-              <button
-                className="text-3-btn"
-                style={{ backgroundColor: Color3 }}
-                onClick={onClick2}
-              ></button>
-            </div>
-            <div className="Color1">
-              {colorpopup2 && (
-                <CirclePicker
-                  Color2={Color2}
-                  onChangeComplete={(Color2) => {
-                    setColor2(Color2.hex);
-                  }}
-                />
-              )}
-            </div>
-            <div className="Text-4">
-              <div className="text-4-1">
-                <p style={{ color: Color }}>Button Border Color</p>
-                <p style={{ color: Color }}>{Color3}</p>
-              </div>
-              <div className="btn">
-                <button className="text-4-btn" onClick={onClick3}></button>
-              </div>
-            </div>
-            <div className="Color1">
-              {colorpopup3 && (
-                <CirclePicker
-                  color3={Color3}
-                  onChangeComplete={(Color3) => {
-                    setColor3(Color3.hex);
-                  }}
-                />
-              )}
-            </div>
-            <div className="Text-5">
-              <div className="text-4-1">
-                <p style={{ color: Color }}>Button Mouse Color</p>
-                <p style={{ color: Color }}>{Color4}</p>
-              </div>
-              <button className="text-5-btn" onClick={onClick4}></button>
-            </div>
-            <div className="Color1">
-              {colorpopup4 && (
-                <CirclePicker
-                  Color4={Color4}
-                  onChangeComplete={(Color4) => {
-                    setColor4(Color4.hex);
-                  }}
-                />
-              )}
-            </div>
+            <button
+              style={{ backgroundColor: Color }}
+              className="text-1-btn"
+              onClick={onClick}
+            ></button>
           </div>
-        )}
-      </div> */}
+          <div className="Color1">
+            {colorpopup && (
+              <CirclePicker
+                color={Color}
+                onChangeComplete={(Color) => {
+                  setColor(Color.hex);
+                }}
+              />
+            )}
+          </div>
+          <div style={{ Color }} className="Text-2">
+            <div className="text-4-1">
+              <p style={{ color: Color }}>Backrground Color</p>
+              <p style={{ color: Color }}>{Color1}</p>
+            </div>
+            <button
+              className="text-2-btn"
+              style={{ backgroundColor: Color1 }}
+              onClick={onClick1}
+            ></button>
+          </div>
+          <div className="Color1">
+            {colorpopup1 && (
+              <CirclePicker
+                Color1={Color1}
+                onChangeComplete={(Color1) => {
+                  setColor1(Color1.hex);
+                }}
+              />
+            )}
+          </div>
+          <div className="Text-3">
+            <div className="text-4-1">
+              <p style={{ color: Color }}>Button Color</p>
+              <p style={{ color: Color }}>{Color2}</p>
+            </div>
+            <button
+              className="text-3-btn"
+              style={{ backgroundColor: Color2 }}
+              onClick={onClick2}
+            ></button>
+          </div>
+          <div className="Color1">
+            {colorpopup2 && (
+              <CirclePicker
+                Color2={Color2}
+                onChangeComplete={(Color2) => {
+                  setColor2(Color2.hex);
+                }}
+              />
+            )}
+          </div>
+          <div style={{ Color }} className="Text-4">
+            <div className="text-4-1">
+              <p style={{ color: Color }}>Button Border Color</p>
+              <p style={{ color: Color }}>{Color3}</p>
+            </div>
+            <button
+              className="text-4-btn"
+              style={{ backgroundColor: Color3 }}
+              onClick={onClick3}
+            ></button>
+          </div>
+          <div className="Color1">
+            {colorpopup3 && (
+              <CirclePicker
+                Color3={Color3}
+                onChangeComplete={(Color3) => {
+                  setColor3(Color3.hex);
+                }}
+              />
+            )}
+          </div>
+          <div className="Text-5">
+            <div className="text-4-1">
+              <p style={{ color: Color }}>Button Mouse Color</p>
+              <p style={{ color: Color }}>{Color4}</p>
+            </div>
+            <button className="text-5-btn" onClick={onClick4}></button>
+          </div>
+          <div className="Color1">
+            {colorpopup4 && (
+              <CirclePicker
+                Color4={Color4}
+                onChangeComplete={(Color4) => {
+                  setColor4(Color4.hex);
+                }}
+              />
+            )}
+          </div>
+        </div>
+      )}
+
       <div className="Button-container">
-        <button className="Cancel-btn" onClick={onClose}>
+        <button  className="Cancel-btn" onClick={onClose}>
           Cancel
         </button>
-        <button className="Save-btn">Save</button>
-      </div>
-    </div>
+        <button style={{ backgroundColor:Color2, borderBlockColor:Color3 }} className="Save-btn">Save</button>
+     </div>
     </div>
   );
 }
