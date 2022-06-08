0747e4a2fbc2e9a0e34effd2328f14957f2730c7
Ebsar <ebsar@Ebsars-MacBook-Pro.local>
2022-06-08 13:22:40 +0200

    deploy

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -35,7 +35,9 @@ function Modal({ onClose }) {
     <div style={{ backgroundColor: Color1 }} className="backrground-container">
       <div className="First-Container">
         <p style={{ color: Color }}>Theme Color</p>
-        <p style={{ color: Color2 }} onClick={onClicker} >Change Theme</p>
+        <p style={{ color: Color2 }} onClick={onClicker}>
+          Change Theme
+        </p>
       </div>
       {ChangeTheme && (
         <div
@@ -47,10 +49,19 @@ function Modal({ onClose }) {
           <div className="Text-1">
             <div className="text-4-1">
               <p style={{ color: Color }}>Font Color</p>
-              <p className="color-f" style={{ color: Color }}>{Color}</p>
+              <p className="color-f" style={{ color: Color }}>
+                {Color}
+              </p>
             </div>
             <button
-              style={{ backgroundColor: Color }}
+            style={{
+              border: "1px solid",
+              borderColor: Color3 ,
+              borderRadius: "5px",
+              backgroundColor:Color,
+              width: "70px",
+              height: "30px",
+            }}
               className="text-1-btn"
               onClick={onClick}
             ></button>
@@ -68,18 +79,27 @@ function Modal({ onClose }) {
           <div style={{ Color }} className="Text-2">
             <div className="text-4-1">
               <p style={{ color: Color }}>Backrground Color</p>
-              <p className="color-f" style={{ color: Color }}>{Color1}</p>
+              <p className="color-f" style={{ color: Color }}>
+                {Color1}
+              </p>
             </div>
             <button
               className="text-2-btn"
-              style={{ backgroundColor: Color1 }}
+              style={{
+                border: "1px solid",
+                borderColor: Color3 ,
+                borderRadius: "5px",
+                backgroundColor:Color1,
+                width: "70px",
+                height: "30px",
+              }}
               onClick={onClick1}
             ></button>
           </div>
           <div className="Color1">
             {colorpopup1 && (
               <CirclePicker
-                Color1={Color1}
+                color1={Color1}
                 onChangeComplete={(Color1) => {
                   setColor1(Color1.hex);
                 }}
@@ -89,18 +109,26 @@ function Modal({ onClose }) {
           <div className="Text-3">
             <div className="text-4-1">
               <p style={{ color: Color }}>Button Color</p>
-              <p className="color-f" style={{ color: Color }}>{Color2}</p>
+              <p className="color-f" style={{ color: Color }}>
+                {Color2}
+              </p>
             </div>
             <button
-              className="text-3-btn"
-              style={{ backgroundColor: Color2 }}
+              className="text-3-btn"style={{
+                border: "1px solid",
+                borderColor: Color3 ,
+                borderRadius: "5px",
+                backgroundColor:Color,
+                width: "70px",
+                height: "30px",
+              }}
               onClick={onClick2}
             ></button>
           </div>
           <div className="Color1">
             {colorpopup2 && (
               <CirclePicker
-                Color2={Color2}
+                color2={Color2}
                 onChangeComplete={(Color2) => {
                   setColor2(Color2.hex);
                 }}
@@ -110,18 +138,26 @@ function Modal({ onClose }) {
           <div style={{ Color }} className="Text-4">
             <div className="text-4-1">
               <p style={{ color: Color }}>Button Border Color</p>
-              <p className="color-f" style={{ color: Color }}>{Color3}</p>
+              <p className="color-f" style={{ color: Color }}>
+                {Color3}
+              </p>
             </div>
             <button
-              className="text-4-btn"
-              style={{ backgroundColor: Color3 }}
+              className="text-4-btn"style={{
+                border: "1px solid",
+                borderColor: Color3 ,
+                borderRadius: "5px",
+                backgroundColor:Color3,
+                width: "70px",
+                height: "30px",
+              }}
               onClick={onClick3}
             ></button>
           </div>
           <div className="Color1">
             {colorpopup3 && (
               <CirclePicker
-                Color3={Color3}
+                color3={Color3}
                 onChangeComplete={(Color3) => {
                   setColor3(Color3.hex);
                 }}
@@ -131,14 +167,23 @@ function Modal({ onClose }) {
           <div className="Text-5">
             <div className="text-4-1">
               <p style={{ color: Color }}>Button Mouse Color</p>
-              <p className="color-f" style={{ color: Color }}>{Color4}</p>
+              <p className="color-f" style={{ color: Color }}>
+                {Color4}
+              </p>
             </div>
-            <button className="text-5-btn" onClick={onClick4}></button>
+            <button className="text-5-btn" style={{
+            border: "1px solid",
+            borderColor: Color3 ,
+            borderRadius: "5px",
+            backgroundColor:Color,
+            width: "70px",
+            height: "30px",
+          }}onClick={onClick4}></button>
           </div>
           <div className="Color1">
             {colorpopup4 && (
               <CirclePicker
-                Color4={Color4}
+                color4={Color4}
                 onChangeComplete={(Color4) => {
                   setColor4(Color4.hex);
                 }}
@@ -149,11 +194,29 @@ function Modal({ onClose }) {
       )}
 
       <div className="Button-container">
-        <button  className="Cancel-btn" onClick={onClose}>
-        <p style={{ color: Color }}  >Cancel</p>
+        <button className="Cancel-btn" onClick={onClose}
+           style={{
+            border: "1px solid",
+            borderColor: Color3 ,
+            borderRadius: "5px",
+            backgroundColor:Color,
+            width: "80px",
+            height: "40px",
+          }}>Cancel
         </button>
-        <button style={{ backgroundColor:Color2, borderBlockColor:Color3, Color:Color }} className="Save-btn"><p style={{ color: Color }}  >Save</p></button>
-     </div>
+        <button
+          style={{
+            border: "1px solid",
+            borderColor: Color3 ,
+            borderRadius: "5px",
+            backgroundColor:Color2,
+            width: "80px",
+            height: "40px",
+          }}
+        >
+          Save
+        </button>
+      </div>
     </div>
   );
 }
